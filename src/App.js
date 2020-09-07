f53ba921581b259151061cd606a6fd5a9c0cd66e
korathbasil <korathbasil@live.com>
2020-09-07 20:50:08 +0530

    login page and authentication via spotify api done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
+import { getTokenFromResponse } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <h2> Lets Build Spotify clone</h2>
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const tokenString = getTokenFromResponse();
+    console.log(token);
+    window.location.hash = "";
+    const _token = tokenString.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+  return <div className="app">{token ? <h2>Bazil</h2> : <Login />}</div>;
 }
 
 export default App;
